05c2a462fcbb0a76f22f795b1bce4533f72965c8
juanifauusti <juanacalzada01@gmail.com>
2025-06-24 11:09:24 -0300

    grupo 9 fixed errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginView from "./views/registerView/registerView";
+import RegisterView from "./views/loginView/loginView";
+import HomeView from "./views/homeView/homeView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginView />} />
+        <Route path="/register" element={<RegisterView/>} />
+        <Route path="/home" element={<HomeView/>} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+export default App;
