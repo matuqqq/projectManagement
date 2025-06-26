2fdaaa68068f89517b194d5c16571d2f4f99caf3
unknown <tig2@itisjt.domisjt.edu.ar>
2025-06-26 10:26:16 -0300

    [GRUPO-7] Update package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,20 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginView from "./views/registerView/registerView";
-import RegisterView from "./views/loginView/loginView";
+import RegisterView from "./views/registerView/registerView";
+import LoginView from "./views/loginView/loginView";
 import HomeView from "./views/homeView/homeView";
 
 function App() {
-  const [channels, setChannels] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // Cambia este ID por el del servidor que quieras mostrar
-  const serverId = "ID_DEL_SERVIDOR"; // <-- PON AQUÍ EL ID REAL
-
-  useEffect(() => {
-    fetch(`http://localhost:3000/api/channels?serverId=${serverId}`)
-      .then(res => res.json())
-      .then(data => {
-        setChannels(data.data || []);
-        setLoading(false);
-      });
-  }, [serverId]);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginView />} />
-        <Route path="/register" element={<RegisterView/>} />
-        <Route path="/home" element={<HomeView/>} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="/home" element={<HomeView />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App;
